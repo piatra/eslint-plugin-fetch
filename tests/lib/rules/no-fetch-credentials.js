3cfc4e4428f81c87c231349d15be270b2b74bc0b
Andrei Oprea <andrei.br92@gmail.com>
2019-03-28 14:31:22 +0100

    no-fetch-credentials

--- tests/lib/rules/no-fetch-credentials.js
+++ tests/lib/rules/no-fetch-credentials.js
@@ -4,7 +4,7 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/no-fetch-default.js");
+const rule = require("../../../lib/rules/no-fetch-credentials.js");
 const RuleTester = require("eslint").RuleTester;
 
 
@@ -16,13 +16,27 @@ const ruleTester = new RuleTester();
 
 ruleTester.run("no-fetch-default", rule, {
   valid: [
-    "fetch(\"http://foo.com\", {})",
+    "fetch(\"http://foo.com\", {credentials: \"omit\"})",
   ],
   invalid: [
     {
       code: "fetch(\"http://foo.com\")",
       errors: [{
         message: "Please provide options for fetch() call",
+        type: "CallExpression",
+      }]
+    },
+    {
+      code: "fetch(bar)",
+      errors: [{
+        message: "Please provide options for fetch() call",
+        type: "CallExpression",
+      }]
+    },
+    {
+      code: "fetch(bar, {})",
+      errors: [{
+        message: "Fetch call does not omit credentials",
         type: "CallExpression",
       }]
     }
