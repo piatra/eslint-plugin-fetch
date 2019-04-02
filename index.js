954a6a4d8edd624d6b54ac657ee8751c1b9873e9
Andrei Oprea <andrei.br92@gmail.com>
2019-04-02 16:29:04 +0200

    Remove dependency on requireindex

--- index.js
+++ index.js
@@ -5,18 +5,7 @@
 "use strict";
 
 //------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
-var requireIndex = require("requireindex");
-
-//------------------------------------------------------------------------------
 // Plugin Definition
 //------------------------------------------------------------------------------
 
-
-// import all rules in lib/rules
-module.exports.rules = requireIndex(__dirname + "/lib/rules");
-
-
-
+module.exports.rules = {"no-fetch-credentials": require(__dirname + "/lib/rules/no-fetch-credentials.js")};
